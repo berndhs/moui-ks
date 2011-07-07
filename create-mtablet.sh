fb0a991ccb07a4d92e9aa58eb06f617a025c4f0d
Bernd Stramm <bernd@tonga.berndnet>
2011-07-07 23:40:04 +0300

    nicer names in little script

--- create-mtablet.sh
+++ create-mtablet.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set +x
+MHOME=${HOME}/mywork/moui/ks
+KS=moui-devel-ia32.ks
+#cp ${MHOME}/${KS} /var/tmp/
 nice -5 sudo mic-image-creator \
-	--pkgmgr=yum \
-	--config=meego-tablet.ks \
+	--config=${MHOME}/${KS} \
         --format=livecd \
 	--cache=mic_cache 
