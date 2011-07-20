f6e2d61ffdd22842066fdcd2e1e6ff7067b84210
Bernd Stramm <bernd.stramm@gmail.com>
2011-07-19 22:15:51 -0400

    normal timeout value

--- create-mini.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set +x
-setenforce 0
-sudo setarch i686 livecd-creator --config=./minimal.ks \
-	--fslabel=miniCD \
-	--cache=/var/cache/live
--- create-mtablet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +x
-MHOME=${HOME}/mywork/moui/ks
-KS=moui-devel-ia32.ks
-#cp ${MHOME}/${KS} /var/tmp/
-nice -5 sudo mic-image-creator \
-	--config=${MHOME}/${KS} \
-        --format=livecd \
-	--cache=mic_cache 
