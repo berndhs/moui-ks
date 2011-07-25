b15d90cc586dc41ec9f4a9fffded91f25585c60b
Bernd Stramm <bernd.stramm@gmail.com>
2011-07-25 10:32:21 -0400

    use yum on fedora

--- create-tablet.sh
+++ create-tablet.sh
@@ -4,6 +4,11 @@ MHOME=${HOME}/mywork/moui/ks
 KS=meego-tablet.ks
 mkdir -p /tmp/moui
 TMPKS=/tmp/moui/${KS}
+if [ -e /etc/fedora-release ]; then
+  PACK_MGR_OPT="--pkgmgr=yum"
+else
+  PACK_MGR_OPT=""
+fi
 
 #BUILD_ID="1.2.80.8.0.20110628.2"
 #sed s/@BUILD_ID@/$BUILD_ID/ ${MHOME}/${KS} > ${TMPKS}
@@ -13,4 +18,5 @@ cp  ${MHOME}/${KS}  ${TMPKS}
 nice -5 sudo mic-image-creator \
 	--config=${TMPKS} \
         --format=livecd \
+	$PACK_MGR_OPT \
 	--cache=mic_cache 
