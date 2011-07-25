e96c84e37be420eb30e41774d81fd8a300cb4a30
Bernd Stramm <bernd.stramm@gmail.com>
2011-07-25 10:58:53 -0400

    more different ks files

---
+++ create-geuzen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MHOME=${HOME}/mywork/moui/ks
+KS=geuzen-tablet.ks
+mkdir -p /tmp/moui
+TMPKS=/tmp/moui/${KS}
+if [ -e /etc/fedora-release ]; then
+  PACK_MGR_OPT="--pkgmgr=yum"
+else
+  PACK_MGR_OPT=""
+fi
+
+#BUILD_ID="1.2.80.8.0.20110628.2"
+#sed s/@BUILD_ID@/$BUILD_ID/ ${MHOME}/${KS} > ${TMPKS}
+
+cp  ${MHOME}/${KS}  ${TMPKS}
+
+echo "using copy of ks file " 
+ls -l ${MHOME}/${KS}
+exit
+nice -5 sudo mic-image-creator \
+	--config=${TMPKS} \
+        --format=livecd \
+	$PACK_MGR_OPT \
+	--cache=mic_cache 
---
+++ geuzen-tablet.ks
@@ -0,0 +1,1 @@
+geuzen-ks/geuzen-tablet-ia32-pinetrail-1.2.0.90.9.20110719.4.ks
